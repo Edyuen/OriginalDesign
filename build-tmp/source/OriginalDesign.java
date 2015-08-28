267ddf7adb17bf206f4884bba594e83b394b088e
unknown <edyuen@697-334-Faramir.sf.k12.internal>
2015-08-28 11:02:46 -0700

    changed

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalDesign extends PApplet {
+
 int starX = (int)(Math.random()*350)+20;
 int starY = (int)(Math.random()*150)+30;
 int starX1 = (int)(Math.random()*350)+20;
@@ -123,12 +139,12 @@ int starYY = (int)(Math.random()*150)+30;
 
 
 
-void setup()
+public void setup()
 {
 	size(400,350);
 }
 
-void draw()
+public void draw()
 {
 	background(-mouseY+150, -mouseY+200, -mouseY+250);
 	Sun();
@@ -138,13 +154,13 @@ void draw()
 	
 }
 
-void Ground()
+public void Ground()
 {
 	fill(178, 113, 69,200);
 	rect(0, 250, 400, 180);
 }
 
-void Sun ()
+public void Sun ()
 {
 	noStroke();
 	fill(255, 196, 58,-mouseY+280);
@@ -152,14 +168,14 @@ void Sun ()
 	
 }
 
-void Moon()
+public void Moon()
 {
 	noStroke();
 	fill(255,255,255, mouseY-50);
 	ellipse(70, 350-mouseY, 50,50);
 }
 
-void stars()
+public void stars()
 {
 
 	int mouzeY=mouseY;
@@ -240,13 +256,22 @@ void stars()
 
 }
 
-void mouseClicked()
+public void mouseClicked()
 {
 	
 
 }
 
-void meteorImage()
+public void meteorImage()
 {
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalDesign" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
